c7cde43eab070230b796c485f6c1db8ca4c7a5de
burgokmen <burgokmen@gmail.com>
2023-10-07 23:14:34 +0300

    Book folder ongoing. Enum Journaltype added

---
+++ src/com/brutech/model/books/Magazines.java
@@ -0,0 +1,7 @@
+package com.brutech.model.books;
+
+public class Magazines extends Book {
+    public Magazines(int bookId, String author, String title, double price, boolean status, String edition, String dateOfPurchase) {
+        super(bookId, author, title, price, status, edition, dateOfPurchase);
+    }
+}
