57cef365fa10fee599c94ce05975aefd106ba842
burgokmen <burgokmen@gmail.com>
2023-10-07 23:15:07 +0300

    main update

---
+++ src/com/brutech/main/Main.java
@@ -0,0 +1,10 @@
+package com.brutech.main;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+// TODO ideas here
+// enum Course has a setter . Librarian could use that setter to add new course types
---
+++ src/com/brutech/model/person/Person.java
@@ -0,0 +1,7 @@
+package com.brutech.model.person;
+
+public abstract class Person {
+    private String name;
+
+    public abstract String whoYouAre();
+}
