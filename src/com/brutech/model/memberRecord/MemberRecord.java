3aff9283a28fb373864fb28bf1aa04af1caf6ae6
burgokmen <burgokmen@gmail.com>
2023-11-30 21:04:49 +0300

    UPDATE record entities

--- src/com/brutech/model/memberRecord/MemberRecord.java
+++ src/com/brutech/model/memberRecord/MemberRecord.java
@@ -1,5 +1,6 @@
 package com.brutech.model.memberRecord;
 
+import com.brutech.model.books.Book;
 import com.brutech.model.enums.MemberType;
 
 import java.time.LocalDate;
@@ -8,14 +9,14 @@ import java.util.Objects;
 public abstract class MemberRecord {
     private int memberId;
     private MemberType type;
-    private LocalDate dateOfMembership;
+    private String dateOfMembership;
     private int numberOfBooksBorrowed = 0;
     private final int maxNumBooksBorrowed = 5;
     private String name;
     private String address;
     private long cellular;
 
-    public MemberRecord(int memberId, MemberType type, LocalDate dateOfMembership, String name, String address, long cellular) {
+    public MemberRecord(int memberId, MemberType type, String dateOfMembership, String name, String address, long cellular) {
         this.memberId = memberId;
         this.type = type;
         this.dateOfMembership = dateOfMembership;
@@ -24,33 +25,6 @@ public abstract class MemberRecord {
         this.cellular = cellular;
     }
 
-    public int getMemberId() {
-        return memberId;
-    }
-
-    public LocalDate getDateOfMembership() {
-        return dateOfMembership;
-    }
-
-    public int getNumberOfBooksBorrowed() {
-        return numberOfBooksBorrowed;
-    }
-
-    public void setNumberOfBooksBorrowed(int numberOfBooksBorrowed) {
-        this.numberOfBooksBorrowed = numberOfBooksBorrowed;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setCellular(long cellular) {
-        this.cellular = cellular;
-    }
 
     public abstract void returnBookMessage();
 
@@ -79,4 +53,70 @@ public abstract class MemberRecord {
                 ", cellular=" + cellular +
                 '}';
     }
+
+    public Book[] getBooksRead() {
+        return new Book[0];
+    }
+
+    public int getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+
+    public MemberType getType() {
+        return type;
+    }
+
+    public void setType(MemberType type) {
+        this.type = type;
+    }
+
+    public String getDateOfMembership() {
+        return dateOfMembership;
+    }
+
+    public void setDateOfMembership(String dateOfMembership) {
+        this.dateOfMembership = dateOfMembership;
+    }
+
+    public int getNumberOfBooksBorrowed() {
+        return numberOfBooksBorrowed;
+    }
+
+    public void setNumberOfBooksBorrowed(int numberOfBooksBorrowed) {
+        this.numberOfBooksBorrowed = numberOfBooksBorrowed;
+    }
+
+    public int getMaxNumBooksBorrowed() {
+        return maxNumBooksBorrowed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public long getCellular() {
+        return cellular;
+    }
+
+    public void setCellular(long cellular) {
+        this.cellular = cellular;
+    }
 }
+
+
--- src/com/brutech/model/memberRecord/StudentRecord.java
+++ src/com/brutech/model/memberRecord/StudentRecord.java
@@ -2,11 +2,9 @@ package com.brutech.model.memberRecord;
 
 import com.brutech.model.enums.MemberType;
 
-import java.time.LocalDate;
-
 public class StudentRecord extends MemberRecord {
 
-    public StudentRecord(int memberId, MemberType type, LocalDate dateOfMembership, String name, String address, long cellular) {
+    public StudentRecord(int memberId, MemberType type, String dateOfMembership, String name, String address, long cellular) {
         super(memberId, type, dateOfMembership, name, address, cellular);
     }
 
